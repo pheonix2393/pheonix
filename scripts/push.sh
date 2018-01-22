249e825600b65e0ef8b7c3ec2eeeaf8e80680c37
pheonix3993 <pheonix3993@protonmail.com>
2018-01-22 18:48:49 -0500

    update

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,22 +5,19 @@ echo -en '\n' >> file.txt
 
 git add -A
 git commit -m "update"
-git push $1 master
 
 git checkout PH-88
 git merge master
-git push $1 PH-88
 
 git checkout PH-89
 git merge master
-git push $1 PH-89 
 
 git checkout PH-90 
 git merge master
-git push $1 PH-90 
 
 git checkout PH-91 
 git merge master
-git push $1 PH-91 
 
 git checkout master
+
+git remote | xargs -L1 git push --all
